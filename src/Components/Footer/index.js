9244ae2271147507c47b7e8791469e12feacd96f
Victor Queiroz <victoorqrz@gmail.com>
2021-11-03 16:49:53 -0300

    Footer structure

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './styles.css';
+import Logo from '../../assets/icons/logo.png';
+import { Link } from 'react-router-dom';
 
 export function Footer(){
 
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-container">
+      <div className="content">
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <span>&copy; Copyright 2021</span>
+      </div>
     </div>
   );
 }
